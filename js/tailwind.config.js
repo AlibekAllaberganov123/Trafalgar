c8f940e69d1d35072029e49677b7f9070f47772d
AlibekAllaberganov123 <inoqdost478@gmail.com>
2025-10-09 10:45:25 +0500

    feat: added home section

---
+++ js/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+	theme: {
+		extend: {
+			backgroundImage: {
+				'card-fon': "url('/img/bg-2.svg')", // Rasmingiz yo'lini kiriting
+			},
+		},
+	},
+	// ... boshqa sozlamalar
+}
